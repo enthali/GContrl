29028f6edad3a8140641487f4310da5e5fefd802
Georg <georg.m.doll@gmail.com>
2025-01-24 20:55:24 +0100

    Play Store Release 28 (New Live)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "de.drachenfels.gcontrl"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "1.0"
+        versionCode = 28
+        versionName = "New Live"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
